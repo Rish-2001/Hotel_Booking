b2f26c2c196e99f1cc742c17107216349f5f8f95
Rish-2001 <121566349+Rish-2001@users.noreply.github.com>
2023-10-17 21:55:40 +0530

    search filter

--- routes/bookingsRoute.js
+++ routes/bookingsRoute.js
@@ -2,7 +2,8 @@ const express = require('express');
 const router = express.Router();
 const Booking = require('../models/booking');
 const Room = require('../models/room');
-
+const { v4: uuidv4 } = require('uuid');
+const stripe=require('stripe')('sk_test_51O1ogCSH9sbFouxB8vBOyDx41cGAhJ23feNZUKgawtcAGPhXtfQ2n1hiYjUSBxCQyboKaADVTO8CRoMPAR4bTCYJ00mJyyzku1')
 
 router.post('/bookroom', async (req, res) => {
   const {
@@ -11,44 +12,70 @@ router.post('/bookroom', async (req, res) => {
     FromDate,
     ToDate,
     totalammount,
-    totaldays
+    totaldays,
+    token
   } = req.body;
 
   try {
-    const newbooking = new Booking({
-      room: room.name,
-      roomid: room._id,
-      userid,
-      FromDate,
-      ToDate,
-      totalammount,
-      totaldays,
-      transactionid: '1234'
-    });
+    
+    const customer=await stripe.customers.create({
+      email:token.email,
+      source:token.id
+    })
+    const payment =await stripe.paymentIntents.create(
+      {
+        amount:totalammount*100,
+        customer:customer.id,
+        currency:'INR',
+        receipt_email:token.email,
+        automatic_payment_methods: {enabled: true},
+        
+        
+      },{
+        idempotencyKey:uuidv4()
+      }
+    )
+   
+    if(payment){
+      
+        const newbooking = new Booking({
+          room: room.name,
+          roomid: room._id,
+          userid,
+          FromDate,
+          ToDate,
+          totalammount,
+          totaldays,
+          transactionid: '1234'
+        });
+    
+        const booking = await newbooking.save();
+        
+        // Updating the current booking in the room model
+        const roomtemp = await Room.findOne({ _id: room._id });
+        
+        roomtemp.currentbookings.push({
+          bookingid: booking._id,
+          FromDate: FromDate,
+          ToDate: ToDate,
+          userid: userid,
+          status: booking.status
+        });
+        
+        // Use markModified to notify Mongoose about the array modification
+        roomtemp.markModified('currentbookings');
+        
+        await roomtemp.save();
+        
+        
+      
+    }
+    
 
-    const booking = await newbooking.save();
-    
-    // Updating the current booking in the room model
-    const roomtemp = await Room.findOne({ _id: room._id });
-    
-    roomtemp.currentbookings.push({
-      bookingid: booking._id,
-      FromDate: FromDate,
-      ToDate: ToDate,
-      userid: userid,
-      status: booking.status
-    });
-    
-    // Use markModified to notify Mongoose about the array modification
-    roomtemp.markModified('currentbookings');
-    
-    await roomtemp.save();
-    
-    res.json({ success: true, message: 'Room Booked Successfully', booking });
+    res.send("Payment Successfull,Your room is booked");
   } catch (error) {
-    console.error('Booking failed:', error);
-    res.status(500).json({ success: false, error: 'Booking failed due to a server error.' });
-  }
+    return res.status(400).json({error});
+  } 
 });
 
 module.exports = router;
